40330315772b7a87216d817dcd8a80c1bada8f9d
Sreexcerpt <sree.excerpt@gmail.com>
2026-03-25 13:11:45 +0530

    updated files and deleted some files

--- app/app/actions/auth-actions.ts
+++
@@ -1,8 +0,0 @@
-// app/actions/auth-actions.ts
-'use server';
-
-import { getAdminSession } from '@/app/lib/auth';
-
-export async function getAdminSessionAction() {
-  return await getAdminSession();
-}
--- app/app/models/Market.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const MarketSchema = new Schema(
-  {},
-  { timestamps: true, collection: "markets", strict: false }
-);
-
-// Use the same model name that you're importing in your API
-export default mongoose.models.Market || mongoose.model("Market", MarketSchema);
